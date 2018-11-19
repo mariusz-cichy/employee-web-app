99b3621862b5ea7247fe701b16a5d07e4157cb8d
Mariusz Cichy <mariusz.cichy@mcpm.pl>
2018-11-19 23:16:55 +0100

    first commit

---
+++ src/main/java/pl/mcpm/employeewebapp/EmployeeWebAppApplication.java
@@ -0,0 +1,12 @@
+package pl.mcpm.employeewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWebAppApplication.class, args);
+	}
+}
